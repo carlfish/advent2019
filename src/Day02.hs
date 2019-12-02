9c5a6281ed9857663f6b0f18abaf423bbe643aaf
Charles Miller <cmiller@pastiche.org>
2019-12-02 22:43:08 +1100

    Distinguish addresses in day2

--- src/Day02.hs
+++ src/Day02.hs
@@ -7,47 +7,50 @@ import Data.Array
 import Control.Monad.State
 
 type Heap = Array Int Int 
-type RuntimeState = (ProgramState, Heap, Int)
+type RuntimeState = (ProgramState, Heap, Addr)
 type Program = State RuntimeState
 
+newtype Addr = Addr Int
+
 data Op
-  = Sum Int Int Int
-  | Mul Int Int Int
+  = Sum Addr Addr Addr
+  | Mul Addr Addr Addr
   | Halt
-  deriving Show
 
 data ProgramState 
   = Reading
   | Running Op
   | Stopped 
   | Failed String
-  deriving Show
 
 run :: Op -> Program ()
 run (Sum ia ib io) = binaryOp (+) ia ib io >> setPS Reading
 run (Mul ia ib io) = binaryOp (*) ia ib io >> setPS Reading
 run           Halt =                          setPS Stopped
 
-binaryOp :: (Int -> Int -> Int) -> Int -> Int -> Int -> Program ()
+binaryOp :: (Int -> Int -> Int) -> Addr -> Addr -> Addr -> Program ()
 binaryOp f ia ib io = writeHeap io =<< (f <$> readHeap ia <*> readHeap ib)
 
-writeHeap :: Int -> Int -> Program ()
-writeHeap i v = modify (\(ps, h, c) -> (ps, h // [(i, v)], c))
+writeHeap :: Addr -> Int -> Program ()
+writeHeap (Addr i) v = modify (\(ps, h, c) -> (ps, h // [(i, v)], c))
 
-readHeap :: Int -> Program Int
-readHeap i = gets (\(ps, h, c) -> h ! i)
+readHeap :: Addr -> Program Int
+readHeap (Addr i) = gets (\(ps, h, c) -> h ! i)
 
 interpret :: Int -> Program ()
-interpret 1  = running =<< withNext3 Sum
-interpret 2  = running =<< withNext3 Mul
+interpret 1  = running =<< next3Addr Sum
+interpret 2  = running =<< next3Addr Mul
 interpret 99 = running Halt
 interpret i  = setPS (Failed ("Unknown opcode: " <> (show i)))
 
 readNext :: Program Int
-readNext = state (\(ps, h, c) -> (h ! c, (ps, h, c + 1)))
+readNext = state (\(ps, h, (Addr c)) -> (h ! c, (ps, h, Addr (c + 1))))
 
-withNext3 :: (Int -> Int -> Int -> a)  -> Program a
-withNext3 f = f <$> readNext <*> readNext <*> readNext
+readNextAddr :: Program Addr
+readNextAddr = Addr <$> readNext
+
+next3Addr :: (Addr -> Addr -> Addr -> a)  -> Program a
+next3Addr f = f <$> readNextAddr <*> readNextAddr <*> readNextAddr
 
 setPS :: ProgramState -> Program ()
 setPS ps = modify (\(_, h, c) -> (ps, h, c))
@@ -68,11 +71,11 @@ runList :: Program() -> [ Int ] -> Either String Int
 runList prelude is = 
   let 
     program = prelude >> runInterpreter 
-    initState = (Reading, listArray (0, length is) is, 0)
+    initState = (Reading, listArray (0, length is) is, Addr 0)
   in evalState program initState
 
 fixInput :: Int -> Int -> Program ()
-fixInput a b = writeHeap 1 a >> writeHeap 2 b
+fixInput a b = writeHeap (Addr 1) a >> writeHeap (Addr 2) b
 
 runPossibilities :: Int -> [ Int ] -> Maybe (Int, Int)
 runPossibilities target is =
